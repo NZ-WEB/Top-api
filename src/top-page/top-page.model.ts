fb01aa9f83ee083552be554e9027a5b0abf892ea
kann1ball <kokki255@icloud.com>
2021-12-07 16:22:19 +0300

    connected modules

--- src/top-page/top-page.model.ts
+++ src/top-page/top-page.model.ts
@@ -1,3 +1,6 @@
+import { Base, TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
+import { prop } from "@typegoose/typegoose";
+
 export enum TopLavelCategory {
   Courses,
   Services,
@@ -5,23 +8,59 @@ export enum TopLavelCategory {
   Products
 }
 
-export class TopPageModel {
-  _id: string;
-  firstCategory: TopLavelCategory;
-  secondCategory: string;
+export class HhData {
+  @prop()
+  count: number;
+
+  @prop()
+  juniorSalary: number;
+
+  @prop()
+  middleSalary: number;
+
+  @prop()
+  seniorSalary: number;
+}
+
+export class TopPageAdvantage {
+  @prop()
   title: string;
+
+  @prop()
+  description: string;
+}
+
+export interface TopPageModel extends Base { }
+
+export class TopPageModel extends TimeStamps {
+
+  @prop({ enum: TopLavelCategory })
+  firstCategory: TopLavelCategory;
+
+  @prop()
+  secondCategory: string;
+
+  @prop({unique: true })
+  alias: string;
+
+  @prop()
+  title: string;
+
+  @prop()
   category: string;
-  hh?: {
-    count: number;
-    juniorSalary: number;
-    middleSalary: number;
-    seniorSalary: number;
-  };
-  advantages: {
-    title: string;
-    description: string;
-  }[];
+
+  @prop({ type: () => HhData })
+  hh?: HhData;
+
+  @prop({ type: () => [TopPageAdvantage] })
+  advantages: TopPageAdvantage[];
+
+  @prop()
   seText: string;
+
+  @prop()
   tagsTitle: string;
+
+  @prop({type: () => [String]})
   tags: string[];
 }
